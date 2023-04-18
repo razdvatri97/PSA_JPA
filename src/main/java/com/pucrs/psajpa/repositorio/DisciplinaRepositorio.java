019b0e221a3ecbec70f59817bed396bd0c4368a8
Josias Melo Vidal <Josias.Vidal@edu.pucrs.br>
2023-04-17 21:19:52 -0300

    correção crud

--- src/main/java/com/pucrs/psajpa/repositorio/DisciplinaRepositorio.java
+++ src/main/java/com/pucrs/psajpa/repositorio/DisciplinaRepositorio.java
@@ -2,10 +2,11 @@ package com.pucrs.psajpa.repositorio;
 
 import com.pucrs.psajpa.entidate.Disciplina;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DisciplinaRepositorio extends JpaRepository<Disciplina, Integer> {
+public interface DisciplinaRepositorio extends CrudRepository<Disciplina, Integer> {
 
     Disciplina findByTurma (int turma);
 
--- src/main/java/com/pucrs/psajpa/repositorio/EstudanteRepositorio.java
+++ src/main/java/com/pucrs/psajpa/repositorio/EstudanteRepositorio.java
@@ -2,10 +2,11 @@ package com.pucrs.psajpa.repositorio;
 
 import com.pucrs.psajpa.entidate.Estudante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EstudanteRepositorio extends JpaRepository<Estudante, Integer> {
+public interface EstudanteRepositorio extends CrudRepository<Estudante, Integer> {
 
     Estudante findByMatricula (Integer matricula);
 }
